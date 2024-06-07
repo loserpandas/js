0e20b2cf47027d788beccb5b0fe2863cd7a3b80f
loserpandas <loser.cyberpandas@gmail.com>
2024-06-07 20:25:53 +0300

    ... main.js

--- main.js
+++ main.js
@@ -1,20 +1,16 @@
-var _0x4e20 = [
-    '\x59\x6E\x4A\x79', '\x49\x45\x55\x4D', '\x4F\x4C\x43\x41', 
-    '\x6E\x4A\x79\x42', '\x78\x4D\x69\x63'
-];
-var _0x5f1d = [4, 0, 3, 1, 2];
+var _0x1e29 = ['\x4D\x6A\x59\x6E', '\x4A\x79\x42\x4F', '\x4C\x43\x41\x78', '\x4D\x69\x63\x6E', '\x49\x45\x55'];
+var _0x5803 = ['\x30\x78\x30', '\x30\x78\x31', '\x30\x78\x32', '\x30\x78\x33', '\x30\x78\x34'];
 
-function _0x3a52(_0x1c3f, _0x392a) {
-    _0x1c3f = _0x1c3f - 0x0;
-    var _0x1db5 = _0x4e20[_0x1c3f];
+function _0x2a3c(_0x35b9) {
+    _0x35b9 = parseInt(_0x5803[_0x35b9], 16);
+    var _0x1db5 = _0x1e29[_0x35b9];
     return _0x1db5;
 }
 
-function _0x6d7e() {
-    var _0x25b9 = "";
-    for (var i = 0; i < _0x5f1d.length; i++) {
-        _0x25b9 += _0x3a52(_0x5f1d[i]);
+function _0x5b12() {
+    var _0x4121 = "";
+    for (var i = 0; i < _0x5803.length; i++) {
+        _0x4121 += _0x2a3c(i);
     }
-    return _0x25b9;
+    return _0x4121;
 }
-
