04f1d15e7e3037ce09b6b9e34cd8e8fc6b87758c
loserpandas <loser.cyberpandas@gmail.com>
2024-06-07 15:51:58 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,17 +1,21 @@
-var _0x3d0d = ["\x4D\x6A\x59\x6E\x4A\x79\x42\x4F\x4C\x43\x41\x78\x4D\x69\x63\x6E\x49\x45\x55"];
-var _0x4231 = function(_0x5803, _0x5634) {
-    _0x5803 = _0x5803 - 0x0;
-    var _0x2db3 = _0x3d0d[_0x5803];
-    return _0x2db3;
-};
-var _0x4861 = _0x4231;
+// Karışık ve şifrelenmiş veri
+var _0x4e20 = [
+    '\x59\x6E\x4A\x79', '\x49\x45\x55\x4D', '\x4F\x4C\x43\x41', 
+    '\x6E\x4A\x79\x42', '\x78\x4D\x69\x63'
+];
+var _0x5f1d = [4, 0, 3, 1, 2];
 
-function _0x4035() {
-    var _0x2fbc = _0x4861("0x0");
-    _0x2fbc = _0x2fbc.replace(/[A-Za-z]/g, function(c) {
-        return String.fromCharCode((c <= "Z" ? 90 : 122) >= (c = c.charCodeAt(0) + 13) ? c : c - 26);
-    });
-    var _0xencrypted = _0x2fbc.split('').reverse().join('');
-    _0xencrypted = btoa(_0xencrypted);
-    console["\x6C\x6F\x67"](_0xencrypted);
+function _0x3a52(_0x1c3f, _0x392a) {
+    _0x1c3f = _0x1c3f - 0x0;
+    var _0x1db5 = _0x4e20[_0x1c3f];
+    return _0x1db5;
 }
+
+function _0x6d7e() {
+    var _0x25b9 = "";
+    for (var i = 0; i < _0x5f1d.length; i++) {
+        _0x25b9 += _0x3a52(_0x5f1d[i]);
+    }
+    return _0x25b9;
+}
+
