c818714b76c42b9ae4d240c2b4b39842cb15cfb0
Samet Genç <sametgenc95@gmail.com>
2024-06-07 15:24:36 +0300

    initial commit (i hope everything will be fine)

---
+++ main.js
@@ -0,0 +1,17 @@
+var _0x3d0d = ["\x4D\x6A\x59\x6E\x4A\x79\x42\x4F\x4C\x43\x41\x78\x4D\x69\x63\x6E\x49\x45\x55"];
+var _0x4231 = function(_0x5803, _0x5634) {
+    _0x5803 = _0x5803 - 0x0;
+    var _0x2db3 = _0x3d0d[_0x5803];
+    return _0x2db3;
+};
+var _0x4861 = _0x4231;
+
+function _0x4035() {
+    var _0x2fbc = _0x4861("0x0");
+    _0x2fbc = _0x2fbc.replace(/[A-Za-z]/g, function(c) {
+        return String.fromCharCode((c <= "Z" ? 90 : 122) >= (c = c.charCodeAt(0) + 13) ? c : c - 26);
+    });
+    var _0xencrypted = _0x2fbc.split('').reverse().join('');
+    _0xencrypted = btoa(_0xencrypted);
+    console["\x6C\x6F\x67"](_0xencrypted);
+}
